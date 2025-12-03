367593823cf36eac7db179aebc512ae5e64c07a0
btmadmin <btmadmin@DESKTOP-7HJ645U>
2025-12-03 09:26:53 +0530

    Opening the browser

--- DEMOWEBSHOP123/src/test/java/com/demowebshop/cart/testscripts/CartModule.java
+++ DEMOWEBSHOP123/src/test/java/com/demowebshop/cart/testscripts/CartModule.java
@@ -1,12 +1,16 @@
 package com.demowebshop.cart.testscripts;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CartModule {
 	@Test
 public void addTocart() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.fb.com");
+		Reporter.log(driver.getTitle());
 		Reporter.log("Product has been added to cart",true);
 }
 }
